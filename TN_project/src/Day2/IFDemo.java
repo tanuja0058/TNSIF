3f46435a906a35e00ea2dde80458a19803c59592
Tanuja <Tanuja@LAPTOP-AH1BJME2>
2025-06-30 20:51:11 +0530

    Day 2

---
+++ TN_project/src/Day2/IFDemo.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class IFDemo {
+
+	public static void main(String[] args) {
+		int x = 9;
+		if(x==9) {
+			System.out.println("Value of x is "+x);
+		}
+		else {
+			System.out.println("value not same");
+		}
+ 
+	}
+
+}
