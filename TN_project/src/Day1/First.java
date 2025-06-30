7c227866a3fd07ef55a82f7a342496bab6d3f1e8
Tanuja <Tanuja@LAPTOP-AH1BJME2>
2025-06-30 20:46:39 +0530

    Day 2

--- TN_project/src/Day1/First.java
+++ TN_project/src/Day1/First.java
@@ -1,10 +1,13 @@
-package basic;
+package Day1;
 
 public class First {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello World");
+       float f1 = 12.34f;
+       double d = (double)f1;
+       System.out.println(d);
 	}
 
 }
