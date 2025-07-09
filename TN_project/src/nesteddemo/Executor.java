68b7e1984da75ac1eac2cd26a11b93fdfe60c08c
Tanuja <Tanuja@LAPTOP-AH1BJME2>
2025-07-09 10:22:25 +0530

    Day 8

---
+++ TN_project/src/nesteddemo/Executor.java
@@ -0,0 +1,12 @@
+package InterfaceType;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		NestedDemo n=new NestedDemo();
+		n.calArea();
+		n.print();
+
+	}
+
+}
---
+++ TN_project/src/nesteddemo/NestedDemo.java
@@ -0,0 +1,17 @@
+package InterfaceType;
+
+public class NestedDemo implements OuterInterface , OuterInterface.InnerInterface{
+
+	@Override
+	public void calArea() {
+		System.out.println("Outer interface ");
+		
+	}
+
+	@Override
+	public void print() {
+	System.out.println("inner interface ");
+		
+	}
+
+}
