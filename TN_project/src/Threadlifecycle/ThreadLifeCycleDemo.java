d029a48af35100105f84941476ebff6de1107733
Tanuja <Tanuja@LAPTOP-AH1BJME2>
2025-07-10 20:26:58 +0530

    Day 12

---
+++ TN_project/src/Threadlifecycle/ThreadLifeCycleDemo.java
@@ -0,0 +1,27 @@
+package Threadlifecycle;
+
+public class ThreadLifeCycleDemo extends Thread {
+	
+	public void run()
+	{
+		System.out.println("in Side run() thread is alive or not ?"
+	+ this.isAlive());
+		
+		int num=0;
+		while(num<4)
+		{
+			num++;
+			System.out.println("num = "+num);
+		}
+		try {
+			sleep(500);
+			System.out.println("in not runnable state, thread is alive or not"
+			+this .isAlive());
+		}
+		catch (InterruptedException e)
+		{
+			System.err.println("thread interrupted ...."+ e.getMessage());
+		}
+	}
+
+}
