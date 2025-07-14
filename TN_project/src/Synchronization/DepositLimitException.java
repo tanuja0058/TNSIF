54f8e797ce98637f381320576e9f6ebe26547d50
Tanuja <Tanuja@LAPTOP-AH1BJME2>
2025-07-14 20:24:12 +0530

    Day 13

---
+++ TN_project/src/Synchronization/DepositLimitException.java
@@ -0,0 +1,13 @@
+package Synchronization;
+
+public class DepositLimitException extends Exception {
+
+	public DepositLimitException()
+	{
+		super("Daily Limit of deposit is exceed.....");
+	}
+	public DepositLimitException(String message )
+	{
+		super (message);
+	}
+}
---
+++ TN_project/src/Synchronization/InsufficientBalanceException.java
@@ -0,0 +1,14 @@
+package Synchronization;
+
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException()
+	{
+		super("Insufficient balance in your account");
+	}
+	
+	public InsufficientBalanceException(String message)
+	{
+		super(message);
+	}
+}
